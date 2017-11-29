a74369383460862a25735170518703f9454bb900
Eduardo Luiz <eduardoluizdude@gmail.com>
2017-11-29 20:07:06 -0200

    correção ranking orderby

--- migrations/20170611223520_aluno.js
+++ migrations/20170611223520_aluno.js
@@ -5,7 +5,7 @@ exports.up = (knex, Promise) => {
     table.string('email').unique().notNullable();
     table.string('telefone').notNullable();
     table.string('objetivo').notNullable();
-    table.integer('pontos');
+    table.integer('pontos').notNullable().defaultTo(0);
     table.integer('id_usuario').references('usuarios.id');
     table.timestamp('criado_em').notNullable().defaultTo(knex.raw('now()'));
   });
